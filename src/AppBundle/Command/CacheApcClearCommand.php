87adc2a8d30b3acf30acac78fe2e2de8cc717f57
Jerzy Lekowski <jerzy@lekowski.pl>
2016-01-29 14:46:10 +0000

    Added Cache APC Clear command

---
+++ src/AppBundle/Command/CacheApcClearCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CacheApcClearCommand extends ContainerAwareCommand
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('cache:apc:clear')
+            ->setDescription('Clear APC cache')
+        ;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        apc_clear_cache();
+        opcache_reset();
+
+        $output->writeln('<info>OK</info>');
+    }
+}
