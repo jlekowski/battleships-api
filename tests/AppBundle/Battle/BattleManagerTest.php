d90f4c0547162fcf7160d661cf265694dfcc1bfd
Jerzy Lekowski <jerzy@lekowski.pl>
2016-02-19 17:21:07 +0000

    Added Battle Manager tests

--- tests/AppBundle/Battle/BattleManagerTest.php
+++ tests/AppBundle/Battle/BattleManagerTest.php
@@ -6,6 +6,7 @@ use AppBundle\Battle\BattleManager;
 use AppBundle\Battle\CoordsInfo;
 use AppBundle\Battle\CoordsInfoCollection;
 use AppBundle\Entity\Event;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class BattleManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,10 +15,15 @@ class BattleManagerTest extends \PHPUnit_Framework_TestCase
      */
     protected $battleManager;
 
+    /**
+     * @var ObjectProphecy
+     */
+    protected $eventRepository;
+
     public function setUp()
     {
-        $eventRepository = $this->prophesize('AppBundle\Entity\EventRepository');
-        $this->battleManager = new BattleManager($eventRepository->reveal());
+        $this->eventRepository = $this->prophesize('AppBundle\Entity\EventRepository');
+        $this->battleManager = new BattleManager($this->eventRepository->reveal());
     }
 
     public function testGetShotResultMiss()
@@ -29,9 +35,47 @@ class BattleManagerTest extends \PHPUnit_Framework_TestCase
         $event->getGame()->willReturn($game);
         $event->getValue()->willReturn('A10');
 
-        $game->getOtherShips()->willReturn(['A1','C2','D2','F2','H2','J2','F5','F6','I6','J6','A7','B7','C7','F7','F8','I9','J9','E10','F10','G10']);
+        $game->getOtherShips()->willReturn(['A1']);
 
         $this->assertEquals(BattleManager::SHOT_RESULT_MISS, $this->battleManager->getShotResult($event->reveal()));
+    }
+
+    public function testGetShotResultHit()
+    {
+        $event = $this->prophesize('AppBundle\Entity\Event');
+        $game = $this->prophesize('AppBundle\Entity\Game');
+
+        $event->getType()->willReturn(Event::TYPE_SHOT);
+        $event->getGame()->willReturn($game);
+        $event->getValue()->willReturn('C2');
+        $event->getPlayer()->willReturn(1);
+
+        $game->getOtherShips()->willReturn(['C2','D2']);
+
+        $this->eventRepository->findForGameByTypeAndPlayer($game, Event::TYPE_SHOT, 1)->willReturn([]);
+
+        $this->assertEquals(BattleManager::SHOT_RESULT_HIT, $this->battleManager->getShotResult($event->reveal()));
+    }
+
+    public function testGetShotResultSunk()
+    {
+        $shotEvent = $this->prophesize('AppBundle\Entity\Event');
+        $event = $this->prophesize('AppBundle\Entity\Event');
+        $game = $this->prophesize('AppBundle\Entity\Game');
+
+        $shotEvent->getValue()->willReturn('D2|hit');
+
+        $event->getType()->willReturn(Event::TYPE_SHOT);
+        $event->getGame()->willReturn($game);
+        $event->getValue()->willReturn('C2');
+        $event->getPlayer()->willReturn(1);
+
+        $game->getOtherShips()->willReturn(['C2','D2']);
+
+        $attackerShots = [$shotEvent];
+        $this->eventRepository->findForGameByTypeAndPlayer($game, Event::TYPE_SHOT, 1)->willReturn($attackerShots);
+
+        $this->assertEquals(BattleManager::SHOT_RESULT_SUNK, $this->battleManager->getShotResult($event->reveal()));
     }
 
     /**
