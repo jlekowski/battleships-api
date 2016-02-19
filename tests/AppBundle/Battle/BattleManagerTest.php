5e25e5077933c0199a47024756b3403ad853815c
Jerzy Lekowski <jerzy@lekowski.pl>
2016-02-19 17:09:31 +0000

    Added Battle Manager tests

--- tests/AppBundle/Battle/BattleManagerTest.php
+++ tests/AppBundle/Battle/BattleManagerTest.php
@@ -5,6 +5,7 @@ namespace AppBundle\Tests\Battle;
 use AppBundle\Battle\BattleManager;
 use AppBundle\Battle\CoordsInfo;
 use AppBundle\Battle\CoordsInfoCollection;
+use AppBundle\Entity\Event;
 
 class BattleManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,6 +18,32 @@ class BattleManagerTest extends \PHPUnit_Framework_TestCase
     {
         $eventRepository = $this->prophesize('AppBundle\Entity\EventRepository');
         $this->battleManager = new BattleManager($eventRepository->reveal());
+    }
+
+    public function testGetShotResultMiss()
+    {
+        $event = $this->prophesize('AppBundle\Entity\Event');
+        $game = $this->prophesize('AppBundle\Entity\Game');
+
+        $event->getType()->willReturn(Event::TYPE_SHOT);
+        $event->getGame()->willReturn($game);
+        $event->getValue()->willReturn('A10');
+
+        $game->getOtherShips()->willReturn(['A1','C2','D2','F2','H2','J2','F5','F6','I6','J6','A7','B7','C7','F7','F8','I9','J9','E10','F10','G10']);
+
+        $this->assertEquals(BattleManager::SHOT_RESULT_MISS, $this->battleManager->getShotResult($event->reveal()));
+    }
+
+    /**
+     * @expectedException \AppBundle\Exception\UnexpectedEventTypeException
+     * @expectedExceptionMessage Incorrect event type provided: test (expected: shot)
+     */
+    public function testGetShotResultThrowsExceptionForNonShotEvent()
+    {
+        $event = $this->prophesize('AppBundle\Entity\Event');
+        $event->getType()->willReturn('test');
+
+        $this->battleManager->getShotResult($event->reveal());
     }
 
     public function testIsSunk()
