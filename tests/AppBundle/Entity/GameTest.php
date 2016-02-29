bbe661d283c691bb2124a2a54202769648b82b13
Jerzy Lekowski <jerzy@lekowski.pl>
2016-02-29 14:36:59 +0000

    Added Game Entity and Entity Subscriber tests

--- tests/AppBundle/Entity/GameTest.php
+++ tests/AppBundle/Entity/GameTest.php
@@ -34,4 +34,172 @@ class GameTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($user2->reveal(), $this->game->getPlayer());
         $this->assertEquals($user1->reveal(), $this->game->getOther());
     }
+
+    public function testGetPlayerNumberAndOtherNumber()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+
+        // no user2, logged user1
+        $this->game->setUser1($user1->reveal());
+        $this->game->setLoggedUser($user1->reveal());
+        $this->assertEquals(1, $this->game->getPlayerNumber());
+        $this->assertEquals(2, $this->game->getOtherNumber());
+
+        // no user2, logged user2
+        $this->game->setLoggedUser($user2->reveal());
+        $this->assertEquals(2, $this->game->getPlayerNumber());
+        $this->assertEquals(1, $this->game->getOtherNumber());
+
+        // user2, logged user2
+        $this->game->setUser2($user2->reveal());
+        $this->assertEquals(2, $this->game->getPlayerNumber());
+        $this->assertEquals(1, $this->game->getOtherNumber());
+
+        // user2, logged user1
+        $this->game->setLoggedUser($user1->reveal());
+        $this->assertEquals(1, $this->game->getPlayerNumber());
+        $this->assertEquals(2, $this->game->getOtherNumber());
+    }
+
+    /**
+     * @expectedException \AppBundle\Exception\UserNotFoundException
+     * @expectedExceptionMessage Logged user has not been set for the game ``
+     */
+    public function testGetPlayerNumberThrowsExceptionWhenLoggedUserMissing()
+    {
+        $this->game->getPlayerNumber();
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Game belongs to other users
+     */
+    public function testGetPlayerNumberThrowsExceptionWhenGameDoesNotBelongToLoggedUser()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+        $user3 = $this->prophesize('AppBundle\Entity\User');
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+        $user3->getId()->willReturn(3);
+
+        $this->game->setUser1($user1->reveal());
+        $this->game->setUser2($user2->reveal());
+        $this->game->setLoggedUser($user3->reveal());
+
+        $this->game->getPlayerNumber();
+    }
+
+    public function testBelongsToUser()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+        $user3 = $this->prophesize('AppBundle\Entity\User');
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+        $user3->getId()->willReturn(3);
+
+        $this->game->setUser1($user1->reveal());
+        $this->game->setUser2($user2->reveal());
+
+        $this->assertTrue($this->game->belongsToUser($user1->reveal()));
+        $this->assertTrue($this->game->belongsToUser($user2->reveal()));
+        $this->assertFalse($this->game->belongsToUser($user3->reveal()));
+    }
+
+    public function testSetUserSetsUserId()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+
+        $this->game->setUser1($user1->reveal());
+        $this->game->setUser2($user2->reveal());
+        $this->assertEquals($user1->reveal(), $this->game->getUser1());
+        $this->assertEquals($user2->reveal(), $this->game->getUser2());
+        $this->assertEquals(1, $this->game->getUserId1());
+        $this->assertEquals(2, $this->game->getUserId2());
+
+        $this->game->setUser1($user2->reveal());
+        $this->game->setUser2($user1->reveal());
+        $this->assertEquals($user2->reveal(), $this->game->getUser1());
+        $this->assertEquals($user1->reveal(), $this->game->getUser2());
+        $this->assertEquals(2, $this->game->getUserId1());
+        $this->assertEquals(1, $this->game->getUserId2());
+    }
+
+    public function testApplyCurrentTimestamp()
+    {
+        $this->assertNull($this->game->getTimestamp());
+
+        $this->game->applyCurrentTimestamp();
+
+        $this->assertInstanceOf('\DateTime', $this->game->getTimestamp());
+        $timeDiff = time() - $this->game->getTimestamp()->getTimestamp();
+        $this->assertTrue($timeDiff < 1 && $timeDiff >=0);
+
+        $now = new \DateTime();
+        $this->game->setTimestamp($now);
+        $this->assertEquals($now, $this->game->getTimestamp());
+    }
+
+    public function testCanJoin()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+        $user3 = $this->prophesize('AppBundle\Entity\User');
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+        $user3->getId()->willReturn(3);
+
+        // game not available
+        $this->game->setTimestamp(new \DateTime(sprintf('-%d seconds', Game::JOIN_LIMIT + 1)));
+        $this->assertFalse($this->game->canJoin($user3->reveal()));
+
+        $this->game->setUser1($user1->reveal());
+        // user already owns the game
+        $this->game->setTimestamp(new \DateTime());
+        $this->assertFalse($this->game->canJoin($user1->reveal()));
+
+        // user can join
+        $this->game->setTimestamp(new \DateTime());
+        $this->assertTrue($this->game->canJoin($user2->reveal()));
+
+        // both users set
+        $this->game->setUser2($user2->reveal());
+        $this->game->setTimestamp(new \DateTime());
+        $this->assertFalse($this->game->canJoin($user3->reveal()));
+    }
+
+    public function testPlayerShipsAndOtherShips()
+    {
+        $user1 = $this->prophesize('AppBundle\Entity\User');
+        $user2 = $this->prophesize('AppBundle\Entity\User');
+        $user1Ships = ['A1'];
+        $user2Ships = ['B2'];
+
+        $user1->getId()->willReturn(1);
+        $user2->getId()->willReturn(2);
+        $this->game->setUser1Ships($user1Ships);
+        $this->game->setUser2Ships($user2Ships);
+        $this->game->setUser1($user1->reveal());
+        $this->game->setUser2($user2->reveal());
+
+        $this->game->setLoggedUser($user1->reveal());
+        $this->assertEquals($user1Ships, $this->game->getPlayerShips());
+        $this->assertEquals($user2Ships, $this->game->getOtherShips());
+
+        $this->game->setLoggedUser($user2->reveal());
+        $this->assertEquals($user2Ships, $this->game->getPlayerShips());
+        $this->assertEquals($user1Ships, $this->game->getOtherShips());
+    }
 }
---
+++ tests/AppBundle/EventSubscriber/EntitySubscriberTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\AppBundle\EventSubscriber;
+
+use AppBundle\EventSubscriber\EntitySubscriber;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class EntitySubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EntitySubscriber
+     */
+    protected $subscriber;
+
+    /**
+     * @var ObjectProphecy
+     */
+    protected $tokenStorage;
+
+    /**
+     * @var ObjectProphecy
+     */
+    protected $validator;
+
+    /**
+     * @var ObjectProphecy
+     */
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->tokenStorage = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage');
+        $this->validator = $this->prophesize('Symfony\Component\Validator\Validator\ValidatorInterface');
+        $this->logger = $this->prophesize('Psr\Log\LoggerInterface');
+
+        $this->subscriber = new EntitySubscriber($this->tokenStorage->reveal(), $this->validator->reveal(), $this->logger->reveal());
+    }
+
+    public function testPreUpdate()
+    {
+        $eventArgs = $this->prophesize('Doctrine\ORM\Event\PreUpdateEventArgs');
+        $eventArgs->getEntity()->willReturn('entity');
+
+        $this->validator->validate('entity', null, ['update'])->shouldBeCalled();
+        $this->subscriber->preUpdate($eventArgs->reveal());
+    }
+
+    public function testPrePersist()
+    {
+        $eventArgs = $this->prophesize('Doctrine\ORM\Event\LifecycleEventArgs');
+        $eventArgs->getEntity()->willReturn('entity');
+
+        $this->validator->validate('entity')->shouldBeCalled();
+        $this->subscriber->prePersist($eventArgs->reveal());
+    }
+
+    public function testPostLoadSetsLoggerForLoggerAwareEntities()
+    {
+        $eventArgs = $this->prophesize('Doctrine\ORM\Event\LifecycleEventArgs');
+        $entity = $this->prophesize('Psr\Log\LoggerAwareInterface');
+        $eventArgs->getEntity()->willReturn($entity);
+
+        $entity->setLogger($this->logger)->shouldBeCalled();
+
+        $this->subscriber->postLoad($eventArgs->reveal());
+    }
+
+    public function testPostLoadSetsLoggedUser()
+    {
+        $eventArgs = $this->prophesize('Doctrine\ORM\Event\LifecycleEventArgs');
+        $entity = $this->prophesize('AppBundle\Entity\Game');
+        $token = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $user = $this->prophesize('AppBundle\Entity\User');
+
+        $eventArgs->getEntity()->willReturn($entity);
+        $this->tokenStorage->getToken()->willReturn($token);
+        $token->getUser()->willReturn($user);
+
+        $entity->setLoggedUser($user)->shouldBeCalled();
+
+        $this->subscriber->postLoad($eventArgs->reveal());
+    }
+
+    /**
+     * @expectedException \AppBundle\Exception\UserNotFoundException
+     * @expectedExceptionMessage User has not been authenticated yet
+     */
+    public function testPostLoadThrowsExceptionWhenTokenNotPresent()
+    {
+        $eventArgs = $this->prophesize('Doctrine\ORM\Event\LifecycleEventArgs');
+        $entity = $this->prophesize('AppBundle\Entity\Game');
+
+        $eventArgs->getEntity()->willReturn($entity);
+        $this->tokenStorage->getToken()->willReturn(null);
+
+        $this->subscriber->postLoad($eventArgs->reveal());
+    }
+}
