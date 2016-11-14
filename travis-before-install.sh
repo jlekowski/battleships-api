93c3babf993b01509a396dee1f646f3a900178ee
Jerzy Lekowski <jerzy@lekowski.pl>
2016-11-14 11:22:22 +0000

    Removed PHP 5.* support from Travis script

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PHP_VERSION =~ 5.[56] ]]
-then
-    yes '' | pecl install apcu-4.0.10
-elif [[ $TRAVIS_PHP_VERSION =~ 7.*|nightly ]]
-then
-    pear config-set preferred_state beta
-    echo 'extension = apc.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    yes '' | pecl install apcu_bc
-fi
+# for PHP7
+pear config-set preferred_state beta
+echo 'extension = apc.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+yes '' | pecl install apcu_bc
 
 cp app/config/parameters.yml.dist app/config/parameters.yml
 sed -i -e 's/database_user:.*/database_user: travis/' app/config/parameters.yml
